eb237d3eff6775dacfada2e195d66cfdbcd14bfd
Ioannis Boutsikas <mgioanni@outlook.com>
2016-06-02 11:25:02 +0300

    Fixes here, fixes there, preparing for demonstration

--- CFM.FeedbackModule/ViewModels/FeedbackDetailsViewModel.cs
+++ CFM.FeedbackModule/ViewModels/FeedbackDetailsViewModel.cs
@@ -5,8 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using CFM.Data.Models;
 using CFM.Infrastructure;
+using CFM.Infrastructure.Constants;
 using CFM.Infrastructure.Repositories;
+using CFM.Infrastructure.Services;
 using Mehdime.Entity;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 
@@ -17,16 +20,24 @@ namespace CFM.FeedbackModule.ViewModels
         private readonly IDbContextScopeFactory _contextFactory;
         private readonly IFeedbackRepository _feedbackRepository;
         private readonly IApplicationCommands _applicationCommands;
+        private readonly ScoringService _scoringService;
 
         private int currentId;
 
+        public DelegateCommand<int?> FeedbackDetailsCommand { get; private set; } 
+
         public FeedbackDetailsViewModel(IDbContextScopeFactory contextFactory, IFeedbackRepository feedbackRepository,
-                IApplicationCommands applicationCommands)
+                IApplicationCommands applicationCommands, ScoringService scoringService)
         {
             _contextFactory = contextFactory;
             _feedbackRepository = feedbackRepository;
             _applicationCommands = applicationCommands;
+            _scoringService = scoringService;
+
+            FeedbackDetailsCommand = new DelegateCommand<int?>(FeedbackDetails);
         }
+
+        
 
         #region Bindable Properties
 
@@ -36,16 +47,37 @@ namespace CFM.FeedbackModule.ViewModels
             get { return _currentFeedback; }
             set { SetProperty(ref _currentFeedback, value); }
         }
+
+        private ICollection<Feedback> _relevantFeedback;
+        public ICollection<Feedback> RelevantFeedback
+        {
+            get { return _relevantFeedback; }
+            set { SetProperty(ref _relevantFeedback, value); }
+        }
+        #endregion
+
+        #region Command Methods
+        private void FeedbackDetails(int? id)
+        {
+            var uriQuery = new NavigationParameters();
+            uriQuery.Add("feedbackId", id.Value);
+            _applicationCommands.NavigateCommand.Execute(ViewNames.FeedbackDetailsView + uriQuery);
+        }
         #endregion
 
         #region INavigationAware Implementation
         public async void OnNavigatedTo(NavigationContext navigationContext)
         {
             currentId = Convert.ToInt32(navigationContext.Parameters["feedbackId"]);
+            CurrentFeedback = null;
             using (_contextFactory.CreateReadOnly())
             {
-                CurrentFeedback = await _feedbackRepository.GetAsync(currentId, i => i.Assignment, i => i.Assignment.Unit,
+                CurrentFeedback = await _feedbackRepository.FindAsync(f => f.Id == currentId, i => i.Assignment, i => i.Assignment.Unit,
                     i => i.Assignment.Unit.Teachers);
+                var temp = await _feedbackRepository.GetAllAsync(i => i.Assignment, i => i.Assignment.Unit,
+                            i => i.Assignment.Unit.Teachers);
+                _scoringService.ScoreList(CurrentFeedback, ref temp);
+                RelevantFeedback = temp;
             }
         }
 
@@ -56,7 +88,7 @@ namespace CFM.FeedbackModule.ViewModels
 
         public void OnNavigatedFrom(NavigationContext navigationContext)
         {
-            CurrentFeedback = new Feedback();
+            CurrentFeedback = null;
         }
         #endregion
     }
--- CFM.Infrastructure/Services/ScoringService.cs
+++ CFM.Infrastructure/Services/ScoringService.cs
@@ -12,13 +12,14 @@ namespace CFM.Infrastructure.Services
         private static readonly double PROF_SCORE = 4f;
         private static readonly double UNIT_SCORE = 5f;
         private static readonly double ASS_SCORE = 6f;
+        private static readonly double ASS_TYPE_SCORE = ASS_SCORE/2f; 
 
         public ScoringService()
         {
                 
         }
 
-        public void GetScore(Feedback feedback, ref ICollection<Feedback> feedbackList)
+        public void ScoreList(Feedback feedback, ref ICollection<Feedback> feedbackList)
         {
             feedbackList.Remove(feedback);
             foreach(var f in feedbackList)
@@ -27,6 +28,9 @@ namespace CFM.Infrastructure.Services
                 if (feedback.Assignment.Id == f.Assignment.Id)
                     f.RelevanceScore += ASS_SCORE;
 
+                if (feedback.Assignment.Type == f.Assignment.Type)
+                    f.RelevanceScore += ASS_TYPE_SCORE;
+
                 if (feedback.Assignment.Unit.Id == f.Assignment.Unit.Id)
                     f.RelevanceScore += UNIT_SCORE;
 
@@ -34,8 +38,9 @@ namespace CFM.Infrastructure.Services
                 if(teachersCount < 0)
                     f.RelevanceScore += PROF_SCORE * teachersCount;
             }
-
-            feedbackList.OrderByDescending(f => f.RelevanceScore);
+            var irrelevantFeedback = feedbackList.Where(f => f.RelevanceScore < 1 || f.Id == feedback.Id);
+            feedbackList = feedbackList.Except(irrelevantFeedback)
+                                       .OrderByDescending(f => f.RelevanceScore).ToList();
         }
     }
 }
